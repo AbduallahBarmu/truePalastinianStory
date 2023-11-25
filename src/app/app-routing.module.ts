6575584d05fb6b5d4e5acf1827052646dafd2cbd
abduallahbarmu <abduallah.imu2@gmail.com>
2023-11-25 14:21:01 +0300

    add routing but not active yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventProfileComponent } from './event/components/event-profile/event-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'event-profile/:id', component:EventProfileComponent} ,
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
